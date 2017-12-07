e4e230ac3a864259890e656ad40a1c20cdc5d006
Ahmed1337 <ahmedhamed523@gmail.com>
2017-12-07 16:21:37 +0200

    Added Jnetpcap library

--- src/wireshark/Wireshark.java
+++ src/wireshark/Wireshark.java
@@ -5,33 +5,84 @@
  */
 package wireshark;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+import org.jnetpcap.packet.PcapPacket;
+import org.jnetpcap.packet.PcapPacketHandler;
 
 /**
  *
  * @author Ahmed
  */
 public class Wireshark extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+//        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+//        
+//        Scene scene = new Scene(root);
+//        
+//        stage.setScene(scene);
+//        stage.show();
+        List<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs  
+        StringBuilder errbuf = new StringBuilder(); // For any error msgs  
+        int r = Pcap.findAllDevs(alldevs, errbuf);
+        if (r == Pcap.ERROR|| alldevs.isEmpty()) {
+            System.err.printf("Can't read list of devices, error is %s", errbuf
+                    .toString());
+            return;
+        }
+
+        System.out.println("Network devices found:");
+
+        int i = 0;
+        for (PcapIf device : alldevs) {
+            String description
+                    = (device.getDescription() != null) ? device.getDescription()
+                    : "No description available";
+            System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);
+        }
+
+        PcapIf device = alldevs.get(2); // We know we have atleast 1 device  
+        System.out
+                .printf("\nChoosing '%s' on your behalf:\n",
+                        (device.getDescription() != null) ? device.getDescription()
+                        : device.getName());
+
+        int snaplen = 64 * 1024;           // Capture all packets, no trucation  
+        int flags = Pcap.MODE_PROMISCUOUS; // capture all packets  
+        int timeout = 10 * 1000;           // 10 seconds in millis  
+        Pcap pcap
+                = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
+
+        if (pcap == null) {
+            System.err.printf("Error while opening device for capture: "
+                    + errbuf.toString());
+            return;
+        }
+
+        PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
+
+            public void nextPacket(PcapPacket packet, String user) {
+
+                System.out.printf("Received packet at %s caplen=%-4d len=%-4d %s\n",
+                        new Date(packet.getCaptureHeader().timestampInMillis()),
+                        packet.getCaptureHeader().caplen(), // Length actually captured  
+                        packet.getCaptureHeader().wirelen(), // Original length   
+                        user // User supplied object  
+                );
+            }
+        };
+        pcap.loop(100, jpacketHandler, "hi");
+
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
 }
