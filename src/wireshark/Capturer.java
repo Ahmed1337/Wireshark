43138c8eb2085d5f1b24318df4106c88e8d0d5e0
Fadi Assem Zarif <maxprestige55@gmail.com>
2017-12-11 04:25:27 +0000

    Merge branch 'master' of https://github.com/Ahmed1337/Wireshark
    # Conflicts:
    #	src/wireshark/Wireshark.java

--- src/wireshark/Capturer.java
+++ src/wireshark/Capturer.java
@@ -15,9 +15,19 @@ import org.jnetpcap.packet.format.FormatUtils;
 import org.jnetpcap.protocol.lan.Ethernet;
 import org.jnetpcap.protocol.network.Arp;
 import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.protocol.network.Icmp;
 import org.jnetpcap.protocol.tcpip.Http;
 import org.jnetpcap.protocol.tcpip.Tcp;
 import org.jnetpcap.protocol.tcpip.Udp;
+import org.pcap4j.core.*;
+import org.pcap4j.util.*;
+import org.pcap4j.sample.*;
+import org.pcap4j.packet.*;
+import org.pcap4j.packet.factory.*;
+import org.pcap4j.packet.namednumber.NamedNumber;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.selector.*;
+import ch.qos.logback.core.joran.spi.*;
 
 /**
  *
@@ -45,6 +55,16 @@ public class Capturer {
         return System.currentTimeMillis() / 1000.0;
     }
 
+    private static DnsPacket getDnsPacket(PcapPacket p){
+        Udp udp = new Udp();
+        if(p.hasHeader(udp)){
+            try{
+                return DnsPacket.newPacket(udp.getPayload(), 0, udp.getPayloadLength());
+            }catch(Exception e){
+                return null;
+            }
+        }else return null;
+    }
     protected List getDevices() {
 
         List devices = new ArrayList();
@@ -73,7 +93,7 @@ public class Capturer {
     }
 
     protected String getDetailedData(int packetNum) {
-        return  ((StringBuilder)detailedView.get(packetNum)).toString();
+        return ((StringBuilder) detailedView.get(packetNum)).toString();
     }
 
     protected void startCapturing(int deviceNum) {
@@ -97,23 +117,26 @@ public class Capturer {
             private Ip4 ip = new Ip4();
             private Arp arp = new Arp();
             private Udp udp = new Udp();
+            private Icmp icmp = new Icmp();
+            private DnsPacket dns;
             private boolean packetCaptured = false;
 
             @Override
             public void nextPacket(PcapPacket packet, String user) {
-                try {
-                    if (!packetCaptured) {
-                        pcap.setTimeout(Pcap.DEFAULT_TIMEOUT);
-                        packetCaptured = true;
-                    }
-                    String protocol = null;
-                    List row = new ArrayList();
-                    if ((packet.hasHeader(arp) && packet.hasHeader(eth)) || (packet.hasHeader(ip) && (packet.hasHeader(tcp) || packet.hasHeader(udp)))) {
-                        detailedView.add(new StringBuilder(packet.toString()));
-                        hexaView.add(packet.toHexdump());
-                        row.add(number++);
-                        row.add((getCurrentTime() - startTimeInSeconds));
-                    }
+
+                if (!packetCaptured) {
+                    pcap.setTimeout(Pcap.DEFAULT_TIMEOUT);
+                    packetCaptured = true;
+                }
+                String protocol = null;
+                List row = new ArrayList();
+                dns = getDnsPacket(packet);
+                if (packet.hasHeader(eth) && (packet.hasHeader(arp) || (packet.hasHeader(ip) && (packet.hasHeader(icmp) || packet.hasHeader(tcp) || packet.hasHeader(udp))))) {
+                    detailedView.add(new StringBuilder(packet.toString()));
+                    hexaView.add(packet.toHexdump());
+                    row.add(number++);
+                    row.add(((int) ((getCurrentTime() - startTimeInSeconds) * 10000)) / 10000.0);
+
                     if (packet.hasHeader(arp) && packet.hasHeader(eth)) {
                         row.add(FormatUtils.mac(eth.source()));
                         row.add(FormatUtils.mac(eth.destination()));
@@ -123,6 +146,9 @@ public class Capturer {
                         row.add(FormatUtils.ip(ip.destination()));
                         if (packet.hasHeader(udp)) {
                             protocol = "UDP";
+                            if(dns != null){
+                                protocol = "DNS";
+                            }
                         } else if (packet.hasHeader(tcp)) {
                             protocol = "TCP";
                             if (packet.hasHeader(http)) {
@@ -133,17 +159,19 @@ public class Capturer {
                                     if (http.getPayload().length < contentLength) {
                                         HttpHandler.initiateHttpPacket(number, tcp.seq(), tcp.getPayloadLength(), contentLength, http.getPayload());
                                     } else {
-                                        protocol = "HTTP";                                     
+                                        protocol = "HTTP";
                                     }
                                 }
                             }
                             String str = HttpHandler.handleForHttpIfExpected(tcp.seq(), tcp.getPayloadLength(), number, tcp.getPayload());
                             if (str != null) {
                                 protocol = "HTTP";
-                                StringBuilder detailedData = (StringBuilder) detailedView.get(detailedView.size() - 1);
-                                detailedData.append("HTTP-reassembly" + str);
+                                StringBuilder detailedData = (StringBuilder) detailedView.get(detailedView.size()-1);
+                                detailedData.append("HTTP-reassembly").append(str);
                                 //System.out.println(str);
                             }
+                        } else if (packet.hasHeader(icmp)) {
+                            protocol = "ICMP";
                         }
                     }
 
@@ -151,11 +179,11 @@ public class Capturer {
                     row.add(packet.getTotalSize());
                     row.add("");
                     controller.addToTable(row);
-                    // JFormatterTextFormatter;
-                    //System.out.println(detailedData);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
                 }
+                
+
+                // JFormatterTextFormatter;
+                //System.out.println(detailedData);
             }
         };
 
